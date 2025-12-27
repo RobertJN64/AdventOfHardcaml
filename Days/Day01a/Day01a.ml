f142e1e38dfb235f2b7cae77f27993e8ecd3e218
RobertJN64 <robertjnies@gmail.com>
2025-12-26 23:41:36 -0500

    clean up module test wrapper

--- Days/Day01a/Day01a.ml
+++ Days/Day01a/Day01a.ml
@@ -7,22 +7,25 @@ let () =
   let sw2 = input "SW2" 1 in
   let sw3 = input "SW3" 1 in
   let sw4 = input "SW4" 1 in
-  let value = Signal.concat_msb [ sw4; sw3; sw2; sw1 ] in
+  let value_1 = Signal.concat_msb [ sw4; sw3; sw2; sw1 ] in
+  let value_2 = Signal.concat_msb [ sw2; sw3; sw4; sw1 ] in
 
-  let seven_seg_A_G = SS_Display.SS_Display.create {value} in
+  let s1_A_G = SS_Display.SS_Display.create {value=value_1} in
+  let s2_A_G = SS_Display.SS_Display.create {value=value_2} in
+  
   let circuit =
     Circuit.create_exn
       ~name:"top"
-      [ 
-        output "S1_A" (Signal.bit seven_seg_A_G.seven_seg_A_G 6);
-        output "S1_B" (Signal.bit seven_seg_A_G.seven_seg_A_G 5);
-        output "S1_C" (Signal.bit seven_seg_A_G.seven_seg_A_G 4);
-        output "S1_D" (Signal.bit seven_seg_A_G.seven_seg_A_G 3);
-        output "S1_E" (Signal.bit seven_seg_A_G.seven_seg_A_G 2);
-        output "S1_F" (Signal.bit seven_seg_A_G.seven_seg_A_G 1);
-        output "S1_G" (Signal.bit seven_seg_A_G.seven_seg_A_G 0);
-      ]
-  in
-
+      (
+        (List.mapi 
+          (fun i name -> output name (Signal.bit s1_A_G.seven_seg_A_G (6 - i)))  (*map the first seven segment display*)
+          [ "S1_A"; "S1_B"; "S1_C"; "S1_D"; "S1_E"; "S1_F"; "S1_G" ]
+        ) 
+        @ 
+        (List.mapi 
+          (fun i name -> output name (Signal.bit s2_A_G.seven_seg_A_G (6 - i)))  (*map the second seven segment display*)
+          [ "S2_A"; "S2_B"; "S2_C"; "S2_D"; "S2_E"; "S2_F"; "S2_G" ]
+        )
+      ) in
   Rtl.output ~output_mode:(Rtl.Output_mode.To_file "verilog_build/top.v") Verilog circuit
   
