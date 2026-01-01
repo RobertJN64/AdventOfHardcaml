c775a9f8dafe92d5d96d52b9e528153b4a29b890
RobertJN64 <robertjnies@gmail.com>
2026-01-01 14:58:30 -0500

    update testbench and template

--- Days/Day12/Day12_tb.ml
+++ Days/Day12/Day12_tb.ml
@@ -1,38 +1,32 @@
 open AdventLib
 open Hardcaml
-(* open Hardcaml_waveterm *)
+open Hardcaml_waveterm
 
 let () =
   let circuit = Day12.day12 () in
   let config = Cyclesim.Config.trace_all in
   let sim = Cyclesim.create ~config circuit in
-  (* let waves, sim = Waveform.create sim in *)
+  let waves, sim = Waveform.create sim in
 
-  (* let out_chan = open_out "waves.vcd" in *)
-  (* let sim = Vcd.wrap out_chan sim in *)
+  let out_chan = open_out "waves.vcd" in
+  let sim = Vcd.wrap out_chan sim in
   let answer = match Cyclesim.lookup_reg_by_name sim "answer" with
   | Some node -> node
   | None -> failwith "answer reg not found" in
 
   Cyclesim.reset sim;
   Cyclesim.cycle sim;
-  (* SerialTB.send_serial_string sim "40x41: 44 53 34 42 33 43\n"; *)
   SerialTB.send_serial_file sim "Days/Day12/Day12_input.txt";
   let answer_value = Cyclesim.Reg.to_int answer in
-  Printf.printf "Answer = %d\n" answer_value;
-
+  
   for _ = 0 to 10000 do
     Cyclesim.cycle sim;
   done;
 
   (* Display waveform in terminal *)
-  (* let rule = Display_rule.[
-    Names {names =[Port_name.of_string "clock"]; wave_format=Some(Wave_format.Bit); alignment=Text_alignment.Left};
-    Names {names =[Port_name.of_string "reset"]; wave_format=Some(Wave_format.Bit); alignment=Text_alignment.Left};
-    Names {names =[Port_name.of_string "auto_counter"]; wave_format=Some(Wave_format.Int); alignment=Text_alignment.Left};
-    Names {names =[Port_name.of_string "manual_counter"]; wave_format=Some(Wave_format.Int); alignment=Text_alignment.Left}
-  ] in *)
-  (* Waveform.print ~wave_width:1 ~display_width:150 waves;
+  Waveform.print ~wave_width:1 ~display_width:150 ~display_height:50 waves;
+
+  Printf.printf "Answer = %d\n" answer_value;
   
-  close_out out_chan *)
+  close_out out_chan
 
--- Days/tpl/DayXX.ml
+++ Days/tpl/DayXX.ml
@@ -1,28 +1,97 @@
 open AdventLib
 open Hardcaml
 open Signal
+open Always
+
+module States = struct
+    type t =
+      | Idle
+      | Width_RCV
+      | Height_RCV
+      | Count_RCV
+      | Process_Counter
+      | Process_Result
+    [@@deriving sexp_of, compare ~localize, enumerate]
+  end
 
 let dayXX () =
   let clock = input "clock" 1 in
   let reset = input "reset" 1 in
   let rx = input "rx" 1 in
 
+  let spec = Reg_spec.create ~clock ~reset () in
+
   let udout = UART_Decoder.UART_Decoder.create {clock; reset; rx} in
-  let active_byte = udout.rx_byte -- "serial_byte" in
+  let rx_strobe = udout.rx_strobe in
+  let rx_byte = udout.rx_byte in
 
-  let bin_to_bcd = Binary_to_BCD.Binary_to_BCD.create {binary_val=active_byte} in
+  let sm = State_machine.create (module States) spec in
+  let grid_dim_bit_width = 8 in
+  let grid_area_bit_width = 2 * grid_dim_bit_width in
+  let grid_width = Variable.reg spec ~width:grid_dim_bit_width in
+  let grid_height = Variable.reg spec ~width:grid_dim_bit_width in
+  let active_counter = Variable.reg spec ~width:grid_dim_bit_width in
+  let answer = Variable.reg spec ~width:10 in
+  let counter_sum = Variable.reg spec ~width:grid_area_bit_width in
+  let grid_area = grid_width.value *: grid_height.value in
+
+  (* only works if grid_dim_bit_width matches size of new_digit*)
+  let shift_in_digit prev_val new_digit = (select (prev_val *: of_int ~width:grid_dim_bit_width 10) (grid_dim_bit_width-1) 0) +: (new_digit -: of_char '0') in
+  
+
+  compile [ sm.switch [
+    ( Idle, [
+        sm.set_next Width_RCV;
+        grid_width <-- zero grid_dim_bit_width;
+        grid_height <-- zero grid_dim_bit_width;
+        active_counter <-- zero grid_dim_bit_width;
+        counter_sum <-- zero grid_area_bit_width
+    ]);
+    ( Width_RCV, [when_ rx_strobe [
+      if_ (rx_byte ==: of_char 'x')
+        [sm.set_next Height_RCV]
+        [grid_width <-- shift_in_digit grid_width.value rx_byte]
+    ]]);
+    ( Height_RCV, [when_ rx_strobe [
+      if_ (rx_byte ==: of_char ':')
+        [sm.set_next Count_RCV]
+        [grid_height <-- shift_in_digit grid_height.value rx_byte]
+    ]]);
+    ( Count_RCV, [when_ rx_strobe [
+      if_ (rx_byte ==: of_char ' ')
+        [sm.set_next Process_Counter]
+        [if_ (rx_byte ==: of_char '\n')
+          [sm.set_next Process_Result]
+          [active_counter <-- shift_in_digit active_counter.value rx_byte]
+        ]
+    ]]);
+    (Process_Counter, [
+        counter_sum <-- counter_sum.value +: uresize active_counter.value grid_area_bit_width;
+        active_counter <-- zero grid_dim_bit_width;
+        sm.set_next Count_RCV
+    ]);
+    (Process_Result, [
+      when_ (select ((counter_sum.value +: uresize active_counter.value grid_area_bit_width) *: of_int ~width:grid_area_bit_width 8) (grid_area_bit_width - 1) 0 <: grid_area)
+        [answer <-- answer.value +: one 10];
+        sm.set_next Idle
+    ])
+  ]];
+
+  let answer_val = answer.value -- "answer" in (* exposes this register for testbench to check against *)
+
+  let bin_to_bcd = Binary_to_BCD.Binary_to_BCD.create {binary_val=answer_val} in
   let bin_to_bcd_val = bin_to_bcd.bcd_val -- "bcd_out" in
-  let mdd = MultiDigitDisplay.MultiDigitDisplay.create {clock; reset; digits=bin_to_bcd_val} 3 in
-(* 
-  let s1_A_G = SS_Display.SS_Display.create {value=(select active_byte 7 4)} in
-  let s2_A_G = SS_Display.SS_Display.create {value=(select active_byte 3 0)} in *)
-
+  let mdd = MultiDigitDisplay.MultiDigitDisplay.create {clock; reset; digits=bin_to_bcd_val} 4 in
   
   Circuit.create_exn
     ~name:"solution"
     [
       output "ss1_A_G" mdd.ss1_A_G.seven_seg_A_G;
       output "ss2_A_G" mdd.ss2_A_G.seven_seg_A_G;
+      output "LED1" (sm.is Idle);
+      output "LED2" (sm.is Width_RCV);
+      output "LED3" (sm.is Height_RCV);
+      output "LED4" (sm.is Count_RCV)
     ]
     
   
